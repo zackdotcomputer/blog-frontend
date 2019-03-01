e7f65b91d512e6188cac070a71357228f8fbff3d
zackzachariah <zack@zacksheppard.com>
2019-03-01 16:27:56 +0000

    Customize with my assets

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,16 +1,17 @@
 module.exports = {
-    siteUrl: `https://gatsby.ghost.org`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://blog.zacksheppard.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Ghost Gatsby Starter`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `A starter template to build amazing static websites with Ghost and Gatsby`, // This allows an alternative site description for meta data for pages.
+    siteTitleMeta: `It's Zack's Blog`, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: `The thoughts of Zack Sheppard`, // This allows an alternative site description for meta data for pages.
 
-    shareImageWidth: 1000, // Change to the width of your default share image
-    shareImageHeight: 523, // Change to the height of your default share image
+    shareImage: `/images/zack-museum.jpg`, // fallback image for meta data. Will be used, when no post/tag/author image specified. 1200x1200 is recommended
+    shareImageWidth: 1200, // Change to the width of your default share image
+    shareImageHeight: 873, // Change to the height of your default share image
 
-    shortTitle: `Ghost`, // Used for App manifest e.g. Mobile Home Screen
+    shortTitle: `Zack's Blog`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
-    backgroundColor: `#e9e9e9`, // Used for Offline Manifest
-    themeColor: `#15171A`, // Used for Offline Manifest
-}
+    backgroundColor: `#212529`, // Used for Offline Manifest
+    themeColor: `#f7fbfc` // Used for Offline Manifest
+};
