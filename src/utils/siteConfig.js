98894dadc64edfda6a108fbf5103f8eb44ba0c67
Zack Sheppard <zack@zacksheppard.com>
2020-01-06 16:51:48 -0500

    Update host

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: `https://blog.zacksheppard.com`, // Site domain. Do not include a trailing slash!
+  siteUrl: `https://blog.zack.computer`, // Site domain. Do not include a trailing slash!
 
   postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
